c64408cd3a1c74d3925c07b677daec670c07e7fa
jagadeesh <502450548@mail.ad.ge.com>
2018-03-05 18:40:57 +0530

    first-commit

---
+++ public/scripts/modules/sample-module/main.js
@@ -0,0 +1,3 @@
+define(['./sample-module', './sample-directive', './sample-filter','./predix-asset-service', './predix-user-service', './predix-view-service','./login-controller','../masterdata-module/techmdata-controller','./services/login-service','../masterdata-module/services/techmdata-service'], function() {
+
+});
